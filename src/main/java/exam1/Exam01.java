6ffe744d0b3769cdfbad16625bc0ec97cf9ee6a9
Tapakorn Wankaew <tapakorn.w@kasikornbank.com>
2017-07-21 15:20:52 +0700

    case1

--- src/main/java/exam1/Exam01.java
+++ src/main/java/exam1/Exam01.java
@@ -3,6 +3,10 @@ package exam1;
 public class Exam01 {
 
     public Exam01(String firstPerson, String secondPerson) {
+        String player1 = firstPerson;
+        String player2 = secondPerson;
+        int scorePlayer1 = 0;
+        int scorePlayer2 = 0;
     }
 
 
@@ -13,7 +17,8 @@ public class Exam01 {
     }
 
     public String getScore() {
-        return null;
+
+        return "Love-All";
     }
 
 }
